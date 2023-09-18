b559405692cecec3f7f6640d434a520fffe694cd
Sererim <danielleymanw@gmail.com>
2023-09-18 20:21:04 +0300

    Final commit for dictionary approuch leetcode thinks that it is wrong.

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -16,9 +16,21 @@ Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]'''
 
 
-def productExceptSelf(self, nums: list[int]) -> list[int]:
-    pass
+def productExceptSelf(nums: list[int]) -> list[int]:
 
+    nums_dict = {i: x for i, x in enumerate(nums)}
+    print(nums_dict.items())
+    temp = [1 for _ in range(len(nums))]
+    val: int = 0
+    
+    for i in nums_dict.keys():
+        val, nums_dict[i] = nums_dict.get(i), 1
+        for x in nums_dict.values():
+            temp[i] *= x
+        nums_dict[i] = val
+ 
+    print(temp)
+    return temp
 
 if __name__ == "__main__":
-    print(productExceptSelf) if productExceptSelf() == [] else None
+    print(productExceptSelf([1,2,3,4])) if productExceptSelf([0,0]) == [0,0] else None
